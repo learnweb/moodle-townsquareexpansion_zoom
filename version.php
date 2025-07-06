caa1a3e42da93c9ac0d6c1ad8da7d52ee6da2b9e
tamaro <tamarowalter@yahoo.de>
2025-07-06 23:53:24 +0200

    fix supported array

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2024070600;
 $plugin->requires = 2022112819;
-$plugin->supported = [401, 404, 405, 500];
+$plugin->supported = [401, 500];
 $plugin->component = 'townsquareexpansion_zoom';
 $plugin->maturity = MATURITY_RC;
 $plugin->release = 'v5.0-rc1';
